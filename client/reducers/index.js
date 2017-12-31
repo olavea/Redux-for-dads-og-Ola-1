747256ccb8a884be60f5bf2f4b5bdb320c107690
Ola Vea <ola@olavea.com>
2017-12-31 18:23:50 +0100

    Copied Wes Bos video 7:

---
+++ client/reducers/index.js
@@ -0,0 +1,15 @@
+import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
+
+import posts from './posts';
+//import comments from './comments';
+
+const rootReducer = combineReducers({posts, routing: routerReducer });
+
+// routing is changes in url
+
+export default rootReducer;
+
+// removed from line 7 ", comments"
+// Video 7 : koden over
+// Video X : koden under
